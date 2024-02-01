15ef72639366cec87260fed7351694d953c2a41b
ali1x <alifuadabbas1@gmail.com>
2024-02-02 01:40:54 +0300

    Added Profile Image and Linkedin Link

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,13 +1,13 @@
-import ProfileImage from './profile.webp'
 import WaveImage from './wavesNegative.svg'
 import AboutImage from './about.svg'
 import ContactImage from './contact.svg'
 import ToolsImage from './tools.svg'
+import ProfileImage from './profile.jpg'
 
 export {
-    ProfileImage,
     WaveImage,
     AboutImage,
     ContactImage,
     ToolsImage,
+    ProfileImage
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,88 +1,18 @@
 import { TypeAnimation } from "react-type-animation"
 import { Fade } from "react-reveal"
 
-import { WaveImage } from "../assets"
+import { WaveImage, ProfileImage } from "../assets"
 
 const Home = () => {
   return (
     <div id="home" className="h-full mt-12 dark:bg-dark-one">
       <div className="flex flex-col justify-start items-center">
         <div className="w-72 h-72 my-4">
-          <svg viewBox="0 0 1000 1000" xmlns="http://www.w3.org/2000/svg">
-            <defs>
-              <filter id="a">
-                <feTurbulence
-                  result="TURBULENCE"
-                  baseFrequency=".07"
-                  seed="1"
-                />
-                <feDisplacementMap
-                  in="SourceGraphic"
-                  in2="TURBULENCE"
-                  scale="20"
-                />
-              </filter>
-              <filter id="b">
-                <feOffset dx="30" result="OFFSET-1" />
-              </filter>
-              <filter id="d">
-                <feOffset dx="20" result="OFFSET-2" />
-              </filter>
-              <filter id="e">
-                <feOffset dy="50" result="OFFSET-3" />
-              </filter>
-              <mask id="c">
-                <path fill="#fff" d="M0 0h1000v1000H0z" />
-                <circle cx="653" cy="448" r="6" filter="url(#a)" />
-                <circle cx="676" cy="644" r="9" filter="url(#a)" />
-                <circle cx="533" cy="628" r="8" filter="url(#a)" />
-                <circle cx="246" cy="456" r="9" filter="url(#a)" />
-                <g filter="url(#b)" mask="url(#c)">
-                  <path
-                    fill="none"
-                    strokeWidth="9"
-                    stroke="#000"
-                    d="M822 710.5Q726.845 913.692 502.5 917"
-                    filter="url(#a)"
-                  />
-                </g>
-                <g filter="url(#d)" mask="url(#c)" transform="scale(.6)">
-                  <path
-                    fill="none"
-                    strokeWidth="6"
-                    stroke="#000"
-                    d="M822 710.5Q721.96 906.134 502.5 917"
-                    filter="url(#a)"
-                  />
-                </g>
-                <g filter="url(#e)">
-                  <path
-                    fill="none"
-                    strokeWidth="5"
-                    stroke="#000"
-                    d="M133 706.5q-114.246-204 3.5-406"
-                    filter="url(#a)"
-                  />
-                </g>
-                <g filter="url(#e)">
-                  <path
-                    fill="none"
-                    strokeWidth="8"
-                    stroke="#000"
-                    d="M509 84.5Q717.181 104.765 825 284"
-                    filter="url(#a)"
-                  />
-                </g>
-              </mask>
-            </defs>
-            <g mask="url(#c)">
-              <path
-                filter="url(#a)"
-                fill="#3b82f6"
-                d="M822 710.5Q743 921 502.5 917T133 706.5Q4 500 136.5 300.5T509 84.5Q749 68 825 284t-3 426.5Z"
-              />
-            </g>
-          </svg>
+          <img
+            src={ProfileImage}
+            alt="ProfileImage"
+            className="rounded-full border-8 border-blue-500 dark:border-dark-four mt-5"
+          />
         </div>
         <Fade bottom>
           <TypeAnimation
@@ -131,6 +61,20 @@ const Home = () => {
                 viewBox="0 0 496 512"
               >
                 <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z" />
+              </svg>
+            </a>
+            <a
+              href="https://www.linkedin.com/in/alidotjs/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 448 512"
+                className="m-2 cursor-pointer dark:fill-dark-four"
+                height="28"
+              >
+                <path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z" />
               </svg>
             </a>
           </div>
